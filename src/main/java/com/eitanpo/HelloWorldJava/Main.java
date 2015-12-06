5813d551d18692d520cd565f66151a896f2e61e9
Ethan Pogrebizsky <eitanpo@gmail.com>
2015-12-06 15:40:19 +0200

    added project files

---
+++ src/main/java/com/eitanpo/HelloWorldJava/Main.java
@@ -0,0 +1,10 @@
+package com.eitanpo.HelloWorldJava;
+
+/**
+ * Created by ethpog01 on 06/12/2015.
+ */
+public final class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
